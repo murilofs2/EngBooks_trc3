1e57a0d04ec1d314a805634ec5ade59325864243
murilo <murilo.fogao@gmail.com>
2019-11-29 02:46:16 -0300

    Updates

---
+++ precache-manifest.5a7b06476f0e70883ea5a9968d0f59da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9b711c64e6ccf41237d32769af003d",
+    "url": "/EngBooks_trc3/index.html"
+  },
+  {
+    "revision": "7cab67c2795a497650dc",
+    "url": "/EngBooks_trc3/static/css/main.e43ffafb.chunk.css"
+  },
+  {
+    "revision": "a4119a64447c4bef353b",
+    "url": "/EngBooks_trc3/static/js/2.c0205fd9.chunk.js"
+  },
+  {
+    "revision": "7cab67c2795a497650dc",
+    "url": "/EngBooks_trc3/static/js/main.04474216.chunk.js"
+  },
+  {
+    "revision": "6aa06f360e8f4d893252",
+    "url": "/EngBooks_trc3/static/js/runtime-main.60f2524e.js"
+  },
+  {
+    "revision": "47052b22f742f2865dbe416b6de7beac",
+    "url": "/EngBooks_trc3/static/media/engbooks.47052b22.png"
+  }
+]);
